bb2ca06d1ae9712eb7393f5edfa793effe2c9242
SHANTO <rabbymd55555@gmail.com>
2025-10-05 19:57:39 +0600

    complete 1st 5 videos

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
-
+import Home from "../Pages/Home";
+import CategoryNews from "../Pages/CategoryNews";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+      },
+    ],
   },
   {
     path: "/auth",
