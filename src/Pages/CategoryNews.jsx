eead208986f7584a90bbe2cb7e178d2fc56f7732
SHANTO <rabbymd55555@gmail.com>
2025-10-06 11:17:21 +0600

    complete 1st 8 videos

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,10 +1,46 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
+import NewsCard from "../Components/NewsCard";
 
 const CategoryNews = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>CategoryNews -{id}</div>;
+  const data = useLoaderData();
+  const [categoryNews, setCategoryNews] = useState([]);
+  useEffect(() => {
+    if (id === "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filterNews = data.filter(
+        (news) => news.others.is_today_pick === true
+      );
+      setCategoryNews(filterNews);
+    } else {
+      const filterNews = data.filter(
+        (news) => news.category_id === parseInt(id)
+      );
+      setCategoryNews(filterNews);
+    }
+  }, [data, id]);
+  console.log(categoryNews);
+  return (
+    <div>
+      <h2 className="font-bold mb-5">
+        {" "}
+        Total <span className="text-secondary">
+          {" "}
+          {categoryNews.length}
+        </span>{" "}
+        news Found
+      </h2>
+
+      <div className="grid grid-cols-1 gap-5">
+        {categoryNews.map((news) => (
+          <NewsCard key={news.id} news={news}></NewsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNews;
